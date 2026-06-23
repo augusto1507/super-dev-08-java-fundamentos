f615fd39971d3bb61ad23f62b3f9c9056a68cce2
augusto1507 <99281591+augusto1507@users.noreply.github.com>
2026-06-23 16:05:49 -0300

    mais alguns exercicios feitos

--- Exercicios2.java
+++ Exercicios2.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 
 public class Exercicios2 {
     public void executar() {
-        exemplo14();
+        exemplo15();
     }
 
     private void exemplo01() {
@@ -263,88 +263,87 @@ public class Exercicios2 {
         boolean vip;
         boolean ano;
 
-        if(cupom.startsWith("PROMO")){
+        if (cupom.startsWith("PROMO")) {
             promo = true;
-        }else  {
+        } else {
             promo = false;
         }
 
         if (cupom.contains("VIP")) {
             vip = true;
-        }else {
+        } else {
             vip = false;
         }
 
         if (cupom.endsWith("2026")) {
             ano = true;
-        }else {
+        } else {
             ano = false;
         }
 
         if (promo == true && vip == true && ano == true) {
             JOptionPane.showMessageDialog(null, "O cupom é valido");
-        }else {
+        } else {
             JOptionPane.showMessageDialog(null, "O cupom é invalido");
         }
 
+    }
+
+    private void exemplo10() {
+        /*
+         * O que deve ser feito:
+         * - Solicitar uma mensagem de atendimento.
+         * - Verificar se a mensagem contém a palavra "urgente".
+         * - Verificar se a mensagem contém a palavra "cancelar".
+         * - Se possuir "urgente", apresentar "Atendimento prioritário".
+         * - Se possuir "cancelar", apresentar "Solicitação de cancelamento".
+         * - Caso não possua nenhuma das palavras, apresentar "Atendimento comum".
+         * - A comparação deve ignorar maiúsculas e minúsculas.
+         */
+
+        String mensagem = JOptionPane.showInputDialog("Digite saua mensagem para o atendimento").toLowerCase();
+
+        String situacao = "";
+
+        if (mensagem.contains("urgente")) {
+            situacao = "Prioritario";
+        } else if (mensagem.contains("cancelar")) {
+            situacao = "cancelado";
+        } else {
+            situacao = "Comun";
+        }
+
+        JOptionPane.showMessageDialog(null, "Situação do atendimento: " + situacao);
 
     }
 
-    private void exemplo10(){
-        /* O que deve ser feito:
-    - Solicitar uma mensagem de atendimento.
-    - Verificar se a mensagem contém a palavra "urgente".
-    - Verificar se a mensagem contém a palavra "cancelar".
-    - Se possuir "urgente", apresentar "Atendimento prioritário".
-    - Se possuir "cancelar", apresentar "Solicitação de cancelamento".
-    - Caso não possua nenhuma das palavras, apresentar "Atendimento comum".
-    - A comparação deve ignorar maiúsculas e minúsculas. */
+    private void exemplo11() {
+        /*
+         * O que deve ser feito:
+         * - Solicitar o nome completo de uma pessoa.
+         * - Remover espaços do começo e do fim.
+         * - Descobrir a posição do primeiro espaço.
+         * - Separar o primeiro nome.
+         * - Separar o restante do nome.
+         * - Apresentar:
+         * a) nome completo tratado
+         * b) primeiro nome
+         * c) restante do nome
+         * d) quantidade total de caracteres
+         */
 
-    String mensagem = JOptionPane.showInputDialog("Digite saua mensagem para o atendimento").toLowerCase();
+        String nomeCompleto = JOptionPane.showInputDialog("Digite o nome completo").trim();
+        int posicaoEspaco = nomeCompleto.indexOf(" ");
+        String parte1 = nomeCompleto.substring(0, posicaoEspaco);
+        String parte2 = nomeCompleto.substring((posicaoEspaco + 1), nomeCompleto.length());
 
-    String situacao = "";
-
-    if (mensagem.contains("urgente")) {
-        situacao = "Prioritario";
-    }else if(mensagem.contains("cancelar")){
-        situacao = "cancelado";
-    }else {
-        situacao = "Comun";
+        JOptionPane.showMessageDialog(null, "Nome completo: " + nomeCompleto +
+                "\nPrimerio nome: " + parte1 +
+                "\nRestante do nome: " + parte2 +
+                "\nQuantidade de caracteres: " + nomeCompleto.length());
     }
 
-    JOptionPane.showMessageDialog(null, "Situação do atendimento: " + situacao);
-
-
-    }
-
-    private void exemplo11(){
-        /* O que deve ser feito:
-    - Solicitar o nome completo de uma pessoa.
-    - Remover espaços do começo e do fim.
-    - Descobrir a posição do primeiro espaço.
-    - Separar o primeiro nome.
-    - Separar o restante do nome.
-    - Apresentar:
-      a) nome completo tratado
-      b) primeiro nome
-      c) restante do nome
-      d) quantidade total de caracteres */
-    
-
-    String nomeCompleto = JOptionPane.showInputDialog("Digite o nome completo").trim();
-    int posicaoEspaco = nomeCompleto.indexOf(" ");
-    String parte1 = nomeCompleto.substring(0, posicaoEspaco);
-    String parte2 = nomeCompleto.substring((posicaoEspaco + 1), nomeCompleto.length());
-
-    JOptionPane.showMessageDialog(null, "Nome completo: " + nomeCompleto + 
-        "\nPrimerio nome: " + parte1 +
-        "\nRestante do nome: " + parte2 +
-        "\nQuantidade de caracteres: " + nomeCompleto.length()
-    );
-    }
-
-
-    private void exemplo12(){
+    private void exemplo12() {
         String opacao = "";
         int quantidadeAlturas = 0;
         double somaAlturas = 0;
@@ -358,104 +357,236 @@ public class Exercicios2 {
 
         double mediaAlturas = somaAlturas / quantidadeAlturas;
         JOptionPane.showMessageDialog(null, "Quantidade de alturas informadas: " + quantidadeAlturas +
-            "\nSoma das alturas: " + somaAlturas +
-            "\nMédia das alturas: " + mediaAlturas
-        );
+                "\nSoma das alturas: " + somaAlturas +
+                "\nMédia das alturas: " + mediaAlturas);
     }
 
-    private void exemplo13(){
-        /*    O que deve ser feito:
-    - Criar um while para registrar 5 corridas de aplicativo.
-    - Para cada corrida, solicitar:
-      a) nome do passageiro
-      b) distância em quilômetros
-      c) tempo em minutos
-    - Classificar a corrida como curta, média ou longa.
-    - Considerar:
-      a) curta: até 5 km
-      b) média: acima de 5 km e até 15 km
-      c) longa: acima de 15 km
-    - Contar quantas corridas longas foram registradas.
-    - Ao final, apresentar a quantidade de corridas longas. */
+    private void exemplo13() {
+        /*
+         * O que deve ser feito:
+         * - Criar um while para registrar 5 corridas de aplicativo.
+         * - Para cada corrida, solicitar:
+         * a) nome do passageiro
+         * b) distância em quilômetros
+         * c) tempo em minutos
+         * - Classificar a corrida como curta, média ou longa.
+         * - Considerar:
+         * a) curta: até 5 km
+         * b) média: acima de 5 km e até 15 km
+         * c) longa: acima de 15 km
+         * - Contar quantas corridas longas foram registradas.
+         * - Ao final, apresentar a quantidade de corridas longas.
+         */
 
-    int i = 0;
-    int quantidadelongas = 0;
-    while (i < 5) {
-        String nome = JOptionPane.showInputDialog("Digite o nome do passageiro");
-        double km = Double.parseDouble(JOptionPane.showInputDialog("Digite a distancia em km"));
-        int minutos = Integer.parseInt(JOptionPane.showInputDialog("Digite o tempo da corrida em minutos"));
+        int i = 0;
+        int quantidadelongas = 0;
+        while (i < 5) {
+            String nome = JOptionPane.showInputDialog("Digite o nome do passageiro");
+            double km = Double.parseDouble(JOptionPane.showInputDialog("Digite a distancia em km"));
+            int minutos = Integer.parseInt(JOptionPane.showInputDialog("Digite o tempo da corrida em minutos"));
 
-        if (km > 15) {
-            quantidadelongas ++;
+            if (km > 15) {
+                quantidadelongas++;
+            }
+
         }
 
+        JOptionPane.showMessageDialog(null, "Qunatidade de corridas longas: " + quantidadelongas);
     }
 
-    JOptionPane.showMessageDialog(null, "Qunatidade de corridas longas: " + quantidadelongas);
-    }
+    private void exemplo14() {
+        /*
+         * O que deve ser feito:
+         * - Criar um menu com while.
+         * - O menu deve ter as opções:
+         * 1 - Registrar visitante
+         * 2 - Mostrar total de visitantes
+         * 3 - Mostrar total de visitantes menores de idade
+         * 4 - Sair
+         * - Enquanto o usuário não escolher a opção 4, o menu deve continuar
+         * aparecendo.
+         * - Quando escolher 1:
+         * - solicitar o nome do visitante
+         * - solicitar a idade do visitante
+         * - contar mais um visitante
+         * - se for menor de idade, contar também como menor de idade
+         * - Quando escolher 2, mostrar o total de visitantes.
+         * - Quando escolher 3, mostrar o total de visitantes menores de idade.
+         * - Quando escolher 4, encerrar o sistema.
+         * - Para opção inválida, mostrar mensagem de erro.
+         */
 
-    private void exemplo14(){
-        /*    O que deve ser feito:
-    - Criar um menu com while.
-    - O menu deve ter as opções:
-      1 - Registrar visitante
-      2 - Mostrar total de visitantes
-      3 - Mostrar total de visitantes menores de idade
-      4 - Sair
-    - Enquanto o usuário não escolher a opção 4, o menu deve continuar aparecendo.
-    - Quando escolher 1:
-      - solicitar o nome do visitante
-      - solicitar a idade do visitante
-      - contar mais um visitante
-      - se for menor de idade, contar também como menor de idade
-    - Quando escolher 2, mostrar o total de visitantes.
-    - Quando escolher 3, mostrar o total de visitantes menores de idade.
-    - Quando escolher 4, encerrar o sistema.
-    - Para opção inválida, mostrar mensagem de erro. */
+        int opcao = Integer.parseInt(JOptionPane.showInputDialog("""
+                1 - Registrar visitante
+                2 - Mostrar total de visitantes
+                3 - Mostrar total de visitantes menores de idade
+                4 - Sair
+                """));
 
-    int opcao = Integer.parseInt(JOptionPane.showInputDialog("""
-            1 - Registrar visitante
-            2 - Mostrar total de visitantes
-            3 - Mostrar total de visitantes menores de idade
-            4 - Sair
-            """));
-
-
-            int totalDeVisitantes = 0;
-            int totalMenoresDeIdade = 0;
+        int totalDeVisitantes = 0;
+        int totalMenoresDeIdade = 0;
         while (opcao != 4) {
             switch (opcao) {
                 case 1:
                     String nome = JOptionPane.showInputDialog("Digite o nome do visitante");
                     int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite a idade do visitante"));
-                    totalDeVisitantes ++;
-                    if(idade < 18){
+                    totalDeVisitantes++;
+                    if (idade < 18) {
                         totalMenoresDeIdade++;
                     }
                     break;
 
                 case 2:
                     JOptionPane.showMessageDialog(null, "Total de visitantes: " + totalDeVisitantes);
-                    
+
                     break;
 
                 case 3:
                     JOptionPane.showMessageDialog(null, "Total de menores de idade: " + totalMenoresDeIdade);
 
-                    break; 
+                    break;
 
                 default:
                     JOptionPane.showMessageDialog(null, "Opção invalida");
-                    
+
             }
 
             opcao = Integer.parseInt(JOptionPane.showInputDialog("""
-            1 - Registrar visitante
-            2 - Mostrar total de visitantes
-            3 - Mostrar total de visitantes menores de idade
-            4 - Sair
-            """));
+                    1 - Registrar visitante
+                    2 - Mostrar total de visitantes
+                    3 - Mostrar total de visitantes menores de idade
+                    4 - Sair
+                    """));
         }
+    }
+
+    private void exemplo15() {
+        /*
+         * O que deve ser feito:
+         * - Solicitar a quantidade de minutos utilizados em uma ligação.
+         * - Utilizar try/catch para tratar erro de conversão.
+         * - Se o número for válido, calcular o valor da ligação considerando R$ 0,75
+         * por minuto.
+         * - Se o valor digitado for inválido, apresentar
+         * "Quantidade de minutos inválida".
+         * - Ao final, apresentar "Processamento finalizado".
+         */
+
+        try {
+            int minutos = Integer.parseInt(JOptionPane.showInputDialog("Digite a duração da ligação em minutos"));
+            double valor = minutos * 0.75;
+            JOptionPane.showMessageDialog(null, "Valor da ligação: " + valor);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Quantidade de minutos invalida");
+        }
+    }
+
+    private void exemplo16() {
+        /*
+         * O que deve ser feito:
+         * - Solicitar a idade de 4 participantes.
+         * - Utilizar for para repetir o cadastro.
+         * - A idade deve ser maior que 0 e menor que 130.
+         * - Enquanto a idade estiver inválida, pedir novamente.
+         * - Utilizar try/catch para evitar erro caso o usuário digite texto.
+         * - Ao final, apresentar uma mensagem informando que as idades foram
+         * cadastradas.
+         */
+
+        for (int i = 0; i < 4; i++) {
+            int idade = 0;
+
+            while (idade < 0 && idade > 130) {
+                try {
+                    idade = Integer.parseInt(JOptionPane.showInputDialog("Digite a idade"));
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Idade invalida");
+                }
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, "Idades cadastradas com sucesso");
+
+    }
+
+    private void exemplo17() {
+        /*
+         * O que deve ser feito:
+         * - Solicitar a quantidade de alunos que participarão de uma excursão.
+         * - Utilizar for para cadastrar essa quantidade.
+         * - Para cada aluno, solicitar:
+         * a) nome
+         * b) idade
+         * c) se possui autorização dos responsáveis
+         * - Contar quantos alunos possuem autorização.
+         * - Contar quantos alunos não possuem autorização.
+         * - Ao final, apresentar:
+         * a) quantidade autorizada
+         * b) quantidade não autorizada
+         */
+
+        int quantidadeAlunos = Integer
+                .parseInt(JOptionPane.showInputDialog("Digite a quantidade de alunos que participarão da excursão"));
+
+        int possuiAutorizacao = 0;
+        int naoPossuiAutorizacao = 0;
+
+        for (int i = 0; i < quantidadeAlunos; i++) {
+            String nome = JOptionPane.showInputDialog("Digite o nome do aluno");
+            int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite a idade do aluno"));
+            boolean autorizacao = Boolean
+                    .parseBoolean(JOptionPane.showInputDialog("Possui autorização?  [true/false]"));
+
+            if (autorizacao == true) {
+                possuiAutorizacao++;
+            } else {
+                naoPossuiAutorizacao++;
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, "Quantidade de alunos que possuem autorização: " + possuiAutorizacao +
+                "\nQuantidade de alunos que não possuem autorização: " + naoPossuiAutorizacao);
+    }
+
+    private void exemplo18() {
+        /*
+         * O que deve ser feito:
+         * - Utilizar for para solicitar 6 distâncias percorridas em caminhadas.
+         * - Somar todas as distâncias.
+         * - Descobrir a maior distância.
+         * - Descobrir a menor distância.
+         * - Calcular a média das distâncias.
+         * - Ao final, apresentar:
+         * a) soma das distâncias
+         * b) maior distância
+         * c) menor distância
+         * d) média das distâncias
+         */
+
+        double soma = 0;
+        double maiorDistancia = 0;
+        double menorDistancia = 9999999;
+        for (int i = 0; i < 6; i++){
+            double distancia = Double.parseDouble(JOptionPane.showInputDialog("Digite a distancia da caminhada"));
+
+            if (distancia > maiorDistancia) {
+                maiorDistancia = distancia;
+            }
+
+            if (distancia < menorDistancia) {
+                menorDistancia = distancia;
+            }
+
+            soma = soma + distancia;
+        }
+
+        double media = (soma / 6);
+
+        JOptionPane. showMessageDialog(null, "Soma das distancias: " + soma +
+            "\nMaior distancia: " + maiorDistancia +
+            "\nMenor distancia: " + menorDistancia +
+            "\nMédia das distancias: " + media
+        );
     }
 
 }
